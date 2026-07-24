96e0ecdc897a49f12609749be3cc18ab6021ee8a
ssbiswal14 <ssbiswal14+@gmail.com>
2026-07-24 17:06:45 +0530

    chore: Add Google Analytics gtag

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-Z8MV34ZXT5" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z8MV34ZXT5');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning className={`${inter.className} bg-[#0D1117] text-white`}>
         {children}
         <Toaster theme="dark" position="bottom-right" richColors />
